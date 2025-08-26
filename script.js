8f8d59d7c5c5b96417e89b62b21cc211a8df477f
khaled-40 <khaledmasud.dev@gmail.com>
2025-08-26 12:12:28 +0600

    finished the login section and started with the home page

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('loginButton').addEventListener('click', function(event){
+    event.preventDefault();
+    const defaultNumber = "01883321846";
+    const defaultPin = "X4F892";
+    const inputNumber = document.getElementById('number').value;
+    const inputPassword = document.getElementById('password').value;
+    if(inputNumber===defaultNumber&&inputPassword===defaultPin){
+        window.location.href="./home.html"
+    }
+})
